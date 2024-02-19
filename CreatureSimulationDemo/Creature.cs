7f6ec4c9a0ed694380405f50dd6bf2b75d831514
mpatnode1 <149105967+mpatnode1@users.noreply.github.com>
2024-02-19 12:47:27 -0600

    creature demo Week 5

---
+++ CreatureSimulationDemo/Creature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CreatureSimulationDemo
+{
+	public class Creature
+	{
+		public Creature()
+		{
+		}
+	}
+}
+
---
+++ CreatureSimulationDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CreatureSimulationDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
